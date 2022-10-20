e5f4407afc400196bf10fd1de5fe7d41e2e194ef
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-10-20 20:29:38 +0530

    Aashi

--- src/dsa/problems/Largest_Prime_Number.java
+++ src/dsa/problems/Largest_Prime_Number.java
@@ -1,2 +1,31 @@
-package dsa.problems;public class Largest_Prime_Number {
+package dsa.problems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Largest_Prime_Number {
+    public static boolean isPrime(int num){
+        for (int i = 2; i < num/2; i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Permutation permutation = new Permutation();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String str = String.valueOf(num);
+        Permutation.printPermutation(str, "");
+        ArrayList<Integer> prime = new ArrayList<>();
+        for (int i: Permutation.resArray){
+            if (isPrime(i)){
+                prime.add(i);
+            }
+        }
+        Collections.sort(prime);
+        System.out.println(prime.get(0));
+    }
 }
--- src/dsa/problems/Permutation.java
+++ src/dsa/problems/Permutation.java
@@ -1,2 +1,29 @@
-package dsa.problems;public class Permutation {
+package dsa.problems;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Permutation {
+    public static ArrayList<Integer> resArray = new ArrayList<>();
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        String str = String.valueOf(num);
+//        printPermutation(str, "");
+//        for (int i: resArray){
+//            System.out.print(i + " ");
+//        }
+//    }
+
+    public static void printPermutation(String str, String res) {
+        if (str.length() == 0){
+            resArray.add(Integer.parseInt(res));
+            return;
+        }
+        for (int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            String temp = str.substring(0, i) + str.substring(i + 1);
+            printPermutation(temp, res + ch);
+        }
+    }
 }
